9c5b6fbd067cd35de1b8a7c194b741908b085548
Madars Bitenieks <madza@madzathemes.com>
2017-02-13 20:28:08 +0200

    fix

--- magazin-extras.php
+++ magazin-extras.php
@@ -76,7 +76,7 @@ function magazin_theme_setup() {
 	add_image_size( 'magazin_625', 625, 625, false );
 	add_image_size( 'magazin_100', 100, 68, true );
 	add_image_size( 'magazin_5', 15, 99, false );
-	if(is_function('fullstory_theme_setup')) {
+	if(function_exists('fullstory_theme_setup')) {
 		add_image_size( 'magazin_1300', 1300, 370, true );
 		add_image_size( 'magazin_1300_5', 26, 8, true );
 	}
