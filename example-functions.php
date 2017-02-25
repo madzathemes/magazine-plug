05d997ed9fa825c9a59e8b11f7ffc4ce6e736b78
Madars Bitenieks <madza@madzathemes.com>
2017-02-25 05:21:52 +0200

    fix

--- example-functions.php
+++ example-functions.php
@@ -469,6 +469,41 @@ function page_metabox() {
 	) );
 
 }
+
+if(function_exists( 'fullstory_theme_setup' )){
+	function page_metabox_full() {
+		$prefix = 'magazin_';
+
+		/**
+		 * Sample metabox to demonstrate each field type included
+		 */
+		$cmb_demo = new_cmb2_box( array(
+			'id'            => $prefix . 'metabox_page_top',
+			'title'         => esc_html__( 'Page Top', 'magazin' ),
+			'object_types'  => array( 'page', ), // Post type
+			// 'show_on_cb' => 'yourprefix_show_if_front_page', // function should return a bool value
+			// 'context'    => 'normal',
+			// 'priority'   => 'high',
+			// 'show_names' => true, // Show field names on the left
+			// 'cmb_styles' => false, // false to disable the CMB stylesheet
+			// 'closed'     => true, // true to keep the metabox closed by default
+			// 'classes'    => 'extra-class', // Extra cmb2-wrap classes
+			// 'classes_cb' => 'yourprefix_add_some_classes', // Add classes through a callback.
+		) );
+
+		$cmb_demo->add_field( array(
+			'name'             => esc_html__( 'Page Top Information', 'magazin' ),
+			'id'               => $prefix . 'page_top',
+			'type'             => 'radio_inline',
+			'show_option_none' => 'Off ',
+			'options'          => array(
+				'on' => esc_html__( 'On', 'magazin' ),
+			),
+		) );
+
+	}
+	add_action( 'cmb2_admin_init', 'page_metabox_full' );
+}
 /**
  * Hook in and add a demo metabox. Can only happen on the 'cmb2_admin_init' or 'cmb2_init' hook.
  */
