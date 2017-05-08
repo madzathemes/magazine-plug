57fdd45d16d2a59b2341a5738697587aa27095c5
Madars Bitenieks <madza@madzathemes.com>
2017-05-08 12:58:30 +0300

    fix

--- shortcodes/s-posts-tabs.php
+++ shortcodes/s-posts-tabs.php
@@ -50,8 +50,8 @@ function mt_pl_views_shares_grid() {
 	if (!empty($view)){ $viewes = $view + $views; $viewes = number_format($viewes); }
 	$shortcode = "";
 	$shortcode .='<div class="post-info">';
-		if($shares!="0" or $shares!=""){ $data .='<span class="poster-shares">'. $shares .' <span class="mt-data-text">'. esc_html($t_shares) .'</span></span>';}
-		if($viewes!="0" or $viewes!=""){ $data .='<span class="poster-views">'. $viewes .' <span class="mt-data-text">'. esc_html($t_views) .'</span></span>';}
+		if($shares!="0" or $shares!=""){ $shortcode .='<span class="poster-shares">'. $shares .' <span class="mt-data-text">'. esc_html($t_shares) .'</span></span>';}
+		if($viewes!="0" or $viewes!=""){ $shortcode .='<span class="poster-views">'. $viewes .' <span class="mt-data-text">'. esc_html($t_views) .'</span></span>';}
 		if (get_comments_number()!="0") { $shortcode .='<span class="poster-comments">'.get_comments_number().'</span>'; }
 	$shortcode .='</div>';
 
