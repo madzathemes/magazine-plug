056c739d77f52df66b0838bf5523f91255972ca4
madzathemes <madza@madzathemes.com>
2017-06-06 02:55:35 +0300

    fix

--- magazin-extras.php
+++ magazin-extras.php
@@ -350,7 +350,7 @@ function get_access_token_mt() {
 
 function magazin_get_shares( $post_id ) {
 $mt_social = get_option( 'socialcountplus_settings');
-		$cache_key = 'magazin_share_cachezz' . $post_id;
+		$cache_key = 'magazin_share_cachezzz' . $post_id;
 
 		$facebook_token = get_option("facebook_token");
 		$facebook_token = get_access_token_mt();
