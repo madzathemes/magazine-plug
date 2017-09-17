cf56596df15cd8d0ff31cfdcf354c36c3855cb74
madzathemes <madza@madzathemes.com>
2017-09-17 18:51:53 +0300

    fix

--- customizer/customizer-ads.php
+++ customizer/customizer-ads.php
@@ -124,6 +124,7 @@ Kirki::add_field( 'magazin_theme_options[bg_ad_top]', array(
 'settings'    => 'magazin_theme_options[bg_ad_top]',
 'label'       => esc_html__( 'YOUR AD CODE', 'rimi' ),
 'section'     => 'bg_ad_top',
+'transport'   => 'postMessage',
 'priority'    => 10,
 'output'      => '.background-ad',
 
