f4f871500ed1c4a73e07d10d26f91e99cae2f477
madzathemes <madza@madzathemes.com>
2017-10-15 19:59:53 +0300

    fix

--- magazin-extras.php
+++ magazin-extras.php
@@ -945,27 +945,27 @@ function mt_share_bottom(){
 function rimi_import_files() {
     return array(
         array(
-            'import_file_name'             => esc_html__( 'Full Demo ( Posts, Style, Page Samples, Images )', 'rimi' ),
+            'import_file_name'             => esc_html__( 'Full Demo', 'rimi' ),
             'local_import_file'            => trailingslashit( get_template_directory() ) . 'inc/import/demo1/demo.xml',
             'local_import_customizer_file' => trailingslashit( get_template_directory() ) . 'inc/import/demo1/customizer.dat',
-						'import_widget_file_url' => trailingslashit( get_template_directory() ) . 'inc/import/demo1/widget_data.json',
+						#'import_widget_file_url' => trailingslashit( get_template_directory() ) . 'inc/import/demo1/widget_data.json',
             'import_notice'                => esc_html__( 'Customize this theme from Appearance/Customize', 'rimi' ),
 						'import_nr'                		 => '1',
         ),
 				array(
-            'import_file_name'             => esc_html__( 'Page Samples', 'rimi' ),
+            'import_file_name'             => esc_html__( 'Only Page Samples', 'rimi' ),
             'local_import_file'            => trailingslashit( get_template_directory() ) . 'inc/import/demo1/pages.xml',
             'import_notice'                => esc_html__( 'Customize this theme from Appearance/Customize', 'rimi' ),
 						'import_nr'                		 => '2',
         ),
 				array(
-            'import_file_name'             => esc_html__( 'Style', 'rimi' ),
+            'import_file_name'             => esc_html__( 'Only Style', 'rimi' ),
             'local_import_customizer_file' => trailingslashit( get_template_directory() ) . 'inc/import/demo1/customizer.dat',
             'import_notice'                => esc_html__( 'Customize this theme from Appearance/Customize', 'rimi' ),
 						'import_nr'                		 => '3',
         ),
 				array(
-            'import_file_name'             => esc_html__( 'Page Samples & Style', 'rimi' ),
+            'import_file_name'             => esc_html__( 'Only Page Samples & Style', 'rimi' ),
 						'local_import_file'            => trailingslashit( get_template_directory() ) . 'inc/import/demo1/pages.xml',
             'local_import_customizer_file' => trailingslashit( get_template_directory() ) . 'inc/import/demo1/customizer.dat',
             'import_notice'                => esc_html__( 'Customize this theme from Appearance/Customize', 'rimi' ),
@@ -979,26 +979,26 @@ $theme = wp_get_theme( 'rimi' );
 if($theme->exists()){
 function magazine_theme_after_import_setup($selected_import) {
 
+	if ( 'Full Demo' === $selected_import['import_file_name'] ) {
+	    // Assign menus to their locations.
+	    $main_menu = get_term_by( 'name', 'Header', 'nav_menu' );
+			$mobile_menu = get_term_by( 'name', 'Mobile Menu', 'nav_menu' );
+			$footer_menu = get_term_by( 'name', 'Footer Menu', 'nav_menu' );
 
-    // Assign menus to their locations.
-    $main_menu = get_term_by( 'name', 'Header', 'nav_menu' );
-		$mobile_menu = get_term_by( 'name', 'Mobile Menu', 'nav_menu' );
-		$footer_menu = get_term_by( 'name', 'Footer Menu', 'nav_menu' );
+	    set_theme_mod( 'nav_menu_locations', array(
+	            'primary' => $main_menu->term_id,
+							'mobile' => $mobile_menu->term_id,
+							'footer_menu' => $main_menu->term_id,
+	        )
+	    );
+			wp_delete_post(1);
+	    // Assign front page and posts page (blog page).
+	    $front_page_id = get_page_by_title( 'Rimi - Homepage' );
 
-    set_theme_mod( 'nav_menu_locations', array(
-            'primary' => $main_menu->term_id,
-						'mobile' => $mobile_menu->term_id,
-						'footer_menu' => $main_menu->term_id,
-        )
-    );
-		wp_delete_post(1);
-    // Assign front page and posts page (blog page).
-    $front_page_id = get_page_by_title( 'Rimi - Homepage' );
+	    update_option( 'show_on_front', 'page' );
+	    update_option( 'page_on_front', $front_page_id->ID );
 
-    update_option( 'show_on_front', 'page' );
-    update_option( 'page_on_front', $front_page_id->ID );
-
-
+	}
 }
 add_action( 'pt-ocdi/after_import', 'function_magazine_theme_after_import_setup' );
 }
