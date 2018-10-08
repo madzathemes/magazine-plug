cee32cf727a82b407285e50f77296c69a6a429e5
Madars Bitenieks <madza@madzathemes.com>
2018-10-08 17:08:25 +0300

    fix

--- magazin-plug.php
+++ magazin-plug.php
@@ -8,3 +8,26 @@ Version: 4.4.5
 Author URI: https://themeforest.net
 */
 include_once ('magazin-extras.php');
+
+Kirki::add_field( 'theme_config_id', array(
+	'type'        => 'typography',
+	'settings'    => 'my_setting',
+	'label'       => esc_attr__( 'Control Label', 'textdomain' ),
+	'section'     => 'section_id',
+	'default'     => array(
+		'font-family'    => 'Roboto',
+		'variant'        => 'regular',
+		'font-size'      => '14px',
+		'line-height'    => '1.5',
+		'letter-spacing' => '0',
+		'color'          => '#333333',
+		'text-transform' => 'none',
+		'text-align'     => 'left',
+	),
+	'priority'    => 10,
+	'output'      => array(
+		array(
+			'element' => 'body',
+		),
+	),
+) );
