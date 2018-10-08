777550f9d89e9b9107a6dae53419f09a7e352fa2
Madars Bitenieks <madza@madzathemes.com>
2018-10-08 06:16:01 +0300

    fix

--- magazin-plug.php
+++ magazin-plug.php
@@ -4,7 +4,7 @@ Plugin Name: Magazine Plug
 Plugin URI: https://themeforest.net
 Description: Magazin Plugins
 Author: Madars Bitenieks
-Version: 4.4.4
+Version: 4.4.5
 Author URI: https://themeforest.net
 */
 include_once ('magazin-extras.php');
